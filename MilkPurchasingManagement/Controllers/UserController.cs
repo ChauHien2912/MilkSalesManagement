3821f6ed9c2b3698e113fb95fc076112fc9b3231
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2024-07-03 14:13:47 +0700

    account function

--- MilkPurchasingManagement/Controllers/UserController.cs
+++ MilkPurchasingManagement/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using Hairhub.API.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MilkPurchasingManagement.Repo.Dtos.Request.Order;
+using MilkPurchasingManagement.Repo.Dtos.Request.User;
+using MilkPurchasingManagement.Repo.Dtos.Response;
 using MilkPurchasingManagement.Repo.Service.OrderService;
 using MilkPurchasingManagement.Repo.Service.UserService;
 using WareHouseManagement.API.Constant;
@@ -35,6 +37,51 @@ namespace MilkPurchasingManagement.Controllers
             {
                 throw new Exception(ex.Message);
             }
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> Login([FromBody] LoginModel login)
+        {
+            var response = await _service.Login(login);
+            if (!response.Success)
+            {
+                return Unauthorized(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] SignUpModel registerModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = await _service.Register(registerModel);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            var response = _service.Logout();
+            return Ok(response);
+        }
+        [HttpPost("changePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
+        {
+            var response = await _service.ChangePassword(model);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Message);
         }
     }
 }
