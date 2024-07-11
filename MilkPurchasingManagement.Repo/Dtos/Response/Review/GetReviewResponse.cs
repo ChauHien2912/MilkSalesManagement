8e143f59235fbc7b583f00ae84ff18694199c6c2
ChauHien2912 <chauhien2912@gmail.com>
2024-07-11 11:24:10 +0700

    new code

--- MilkPurchasingManagement.Repo/Dtos/Response/Review/GetReviewResponse.cs
+++ MilkPurchasingManagement.Repo/Dtos/Response/Review/GetReviewResponse.cs
@@ -8,6 +8,9 @@ namespace MilkPurchasingManagement.Repo.Dtos.Response.Review
 {
     public class GetReviewResponse
     {
-
+        public string? Content { get; set; }
+        public int? Rate { get; set; }
+        public int? UserId { get; set; }
+        public int? ProductId { get; set; }
     }
 }
--- MilkPurchasingManagement/obj/Debug/net6.0/MilkPurchasingManagement.AssemblyInfo.cs
+++ MilkPurchasingManagement/obj/Debug/net6.0/MilkPurchasingManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MilkPurchasingManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8736bfe7d1d4a7a4e6a34af355071f9894e579f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56604304b4e0d932db6b4457d856f6abe2f81a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MilkPurchasingManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MilkPurchasingManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
